54d9e7d4bee06a62478d558920988657ba28c586
yangbo1987 <752098131@qq.com>
2018-09-20 15:04:47 +0800

    v1

---
+++ src/main/java/com/spring/cloud/es/SpringBootAdminApplication.java
@@ -0,0 +1,16 @@
+package com.spring.cloud.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
